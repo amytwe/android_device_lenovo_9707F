5cd3478303866496224d588200c49711e9700ecf
hyusang <97371126+cloudsweets@users.noreply.github.com>
2023-08-31 18:40:38 +0900

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -5,7 +5,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-DEVICE_PATH := device/lenovo/TB-9707F
+DEVICE_PATH := device/lenovo/9707F
 
 # For building with minimal manifest
 ALLOW_MISSING_DEPENDENCIES := true

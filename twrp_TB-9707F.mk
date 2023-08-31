b8810e28d80aec45c71f775b0d95e72d24cea7bc
Cloudsweets <hyusang328@gmail.com>
2023-08-31 18:00:28 +0900

    update

--- twrp_TB-9707F.mk
+++ twrp_TB-9707F.mk
@@ -5,18 +5,14 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+# Inherit some common TWRP stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from TB-9707F device
 $(call inherit-product, device/lenovo/TB-9707F/device.mk)
 
 PRODUCT_DEVICE := TB-9707F
-PRODUCT_NAME := omni_TB-9707F
+PRODUCT_NAME := twrp_TB-9707F
 PRODUCT_BRAND := Lenovo
 PRODUCT_MODEL := Lenovo TB-9707F
 PRODUCT_MANUFACTURER := lenovo
